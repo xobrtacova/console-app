7449ef2b96bff9361c2b97734776381ad1a1c591
xobrtacova <Tanicka959@gmail.com>
2017-09-19 09:40:59 +0200

    druhzy commit

--- main.cpp
+++ main.cpp
@@ -5,6 +5,17 @@
 
 int main()
 {
+	char text;
+	int prepinac;
+
+	std::cout << "Napis text: ";
+	std::cin >> text;
+
+	std::cout << "Zvol prepinac 1 - c/ 2 - w/3 - l: ";
+	std::cin >> prepinac;
+ 
+// pouzila by som if dalej 
+
 	std::cout << "Hello Zadanie!" << std::endl;
 
 	return 0;
